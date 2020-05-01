6ba44c26a270468c5839783702bcb11383916539
Christos Plachouras <35671409+chrispla@users.noreply.github.com>
2020-05-01 22:19:54 +0400

    merge and significance need debug

--- quantizedencoding.py
+++ quantizedencoding.py
@@ -32,40 +32,50 @@ while (freqs[idx] < freq_max):
     freqs.append(freqn)
     idx += 1
 freq_list_len = len(freqs)
-print(freq_list_len)
-print(freqs)
+print("freqs length :",freq_list_len)
 
 #Traverse all paths
 for i in range(len(all_paths)):
     
+    print("i :", i)
     #For every file
     with open(all_paths[i]) as f:
         content = f.readlines() #read file into list of lines
-        
+        print(len(content))
         #QUANTIZATION
         
         #For every line
-        for j in range(len(content)):
+        j = 0
+        new_content = []
+        while (True):
             
-            freq = float(content[j].rstrip())
-            
-            #remove 0 and over freq_max (and negative, if they exist) values
-            if ((freq <= 0) or (freq > freq_max)):
-                content.remove(j)
+            if (j<len(content)):
+                
+                freq = float(content[j].rstrip())
+                
+                #remove 0 and over freq_max (and negative, if they exist) values
+                if (freq > 0 and freq<freq_max):
+                    #find closest value to quantize to (should be implemented more efficiently in the future)
+                    idx = 0
+                    while (freq > freqs[idx]):
+                        idx+=1
+                    #freq is freqs[idx-1] and freqs[idx], find closer
+                    min_dif = min(freq-freqs[idx-1], freqs[idx]-freq)
+                    if (min_dif == freq-freqs[idx-1]):
+                        #content[j] = str(round(freqs[idx-i], 1))+ "\n"
+                        new_content.append(str(freqs[idx-i])+ "\n")
+                    else:
+                        #content[j] = str(round(freqs[idx], 1))+ "\n"
+                        new_content.append(str(freqs[idx])+ "\n")
+                j+=1
+
             else:
-                #find closest value to quantize to (should be implemented more efficiently in the future)
-                idx = 0
-                while (freq < freqs[idx]):
-                    idx+=1
-                #freq is freqs[idx-1] and freqs[idx], find closer
-                min_dif = min(freq-freq[idx-1], freq[idx]-freq)
-                if (min_dif == freq-freq[idx-1]):
-                    content[j] = str(round(freq[idx-i], 1))+ "\n"
-                else:
-                    content[j] = str(round(freq[idx], 1))+ "\n"
+                #break if out of bounds
+                break
+            
                 
         #ENTRY MERGE 
-                    
+            
         #count consecutive entries
         consec = 1
         
@@ -76,29 +86,33 @@ for i in range(len(all_paths)):
         #For every line
         idx = 0
         while (True):
-            if (idx+1 <= len(content)):  
-                if (content[idx] == content[idx+1]):
+            if (idx+1 < len(new_content)):  
+                if (new_content[idx] == new_content[idx+1]):
                     #if next entry same as current, remove and keep track of streak
-                    content.remove(idx+1)
+                    del new_content[idx+1]
                     consec+=1
                 else:
                     #if next line different, alter current entry, increment idx, end streak
-                    n_line = (content[j].rstrip()) + "," + str(consec) + "\n"
+                    n_line = (new_content[idx].rstrip()) + "," + str(consec) + "\n"
                     idx+=1
                     if (consec > max_consec):
                         max_consec = consec
                     if (consec < min_consec):
                         min_consec = consec
                     consec = 1
+            else:
+                #break if out of bounds
+                break
         
         #SIGNIFICANCE VALUE
         
         dif = max_consec - min_consec
         
         #For every line
-        for j in range(len(content)):
+        for j in range(len(new_content)):
             
-            c_line = content[j].split(",")
+            c_line = new_content[j].split(",")
+            print(j,c_line)
             c_line[1] = int(c_line[1].rstrip())
             if (c_line[1] <= (min_consec + (0.25*dif))):
                 c_line[1] = 1
@@ -107,7 +121,7 @@ for i in range(len(all_paths)):
             elif (c_line[1] >= (max_consec - (0.25*dif))):
                 c_line[1] = 3
             
-            content[j] = c_line[0] + "," + str(c_line[1]) + "\n"
+            new_content[j] = c_line[0] + "," + str(c_line[1]) + "\n"
             
     #WRITE TO NEW FILE
             
